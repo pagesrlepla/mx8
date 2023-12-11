31031a5db16e714d32b3720a8c469c68d453c292
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2023-12-12 01:03:41 +0700

    Create mine_karlsen.sh

---
+++ mine_karlsen.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+POOL=de.karlsen.herominers.com:1195
+WALLET=karlsen:qr8cynv2mfke66huxyutka06977z6e84dfvqhcmw2s03w5zzgp6t7m6exjq4t.lolMinerWorker
+
+./lolMiner --algo KARLSEN --pool $POOL --user $WALLET $@
