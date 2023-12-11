05246834b101d8975fb27d23fc750f71977f006a
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2023-12-12 03:29:58 +0700

    Update and rename mine_karlsen.sh to mine_fishhash.sh

--- mine_karlsen.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-POOL=de.karlsen.herominers.com:1195
-WALLET=karlsen:qr8cynv2mfke66huxyutka06977z6e84dfvqhcmw2s03w5zzgp6t7m6exjq4t.lolMinerWorker
-
-./bash --algo KARLSEN --pool $POOL --user $WALLET $@
