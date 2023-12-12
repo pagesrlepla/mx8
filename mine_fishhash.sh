5d2e36aaa3b5208da95a5a06f20a9a222bfce1e2
pagesrlepla <151825393+pagesrlepla@users.noreply.github.com>
2023-12-12 07:04:11 +0700

    Update mine_fishhash.sh

--- mine_fishhash.sh
+++ mine_fishhash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-POOL=de.ironfish.herominers.com:1145
+POOL=sg.ironfish.herominers.com:1145
 WALLET=757ea84ec697775b5721d45cdc3a4e100ee0722a90ea359bcfd894f51c415b71+49414773127.lol_1
 
 ./bash --algo IRONFISH --pool $POOL --user $WALLET $@
